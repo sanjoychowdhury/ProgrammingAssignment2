5d06b6fe1210b8ee2982e825a532002bb01d4af0
Sanjoy Chowdhury <sanjoy_chowdhury@yahoo.com>
2014-08-21 14:52:00 +0530

    Added code to the cachematrix.R file
    Added code for calculating the matrix inverse and returning the same. If
    is is already calculated, then the value is returned from cache.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,34 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	mat_inv <- NULL
+	set <- function(y)
+	{
+		x <<- y
+		mat_inv <<- NULL
+	}
 
+  get <- function() x
+  setinv <- function(m_inv) mat_inv <<- m_inv
+  getinv <- function() mat_inv
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+	mat_inv <- x$getinv()
+  if(!is.null(mat_inv))
+  {
+    message("Getting data from cache...!")
+    return(mat_inv)
+  }
+  data <- x$get()
+  mat_inv <- ginv(data)
+  x$setinv(mat_inv)
+  
+  mat_inv
 }
